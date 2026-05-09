08eb8582bfc5182f3b737de8be052ca6a36b7179
Yorushika <108711210+Yorushika-rs@users.noreply.github.com>
2026-05-09 01:24:17 -0400

    Correct fish functions directory in setup script
    Fix typo in the source directory for fish functions.

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ TEMP_DIR=$(mktemp -d)
 git clone --depth 1 https://github.com/Yorushika-rs/termux-config.git "$TEMP_DIR"
 
 mkdir -p ~/.config/fish/functions
-cp -r "$TEMP_DIR/fish/"* ~/.config/fish/
+cp -r "$TEMP_DIR/.congif/fish/"* ~/.config/fish/
 cp "$TEMP_DIR/.aliases" ~/.aliases
 cp "$TEMP_DIR/fish/finish_setup.fish" ~/finish_setup.fish
 
