7a7e2a96e656d186f6d39dee9f86dedc550c1efb
Yorushika <108711210+Yorushika-rs@users.noreply.github.com>
2026-05-09 01:15:04 -0400

    Modify setup.sh for fish functions and execution
    Updated setup script to create functions directory and execute fish shell.

--- setup.sh
+++ setup.sh
@@ -6,15 +6,12 @@ pkg install -y git fish python curl wget fzf proot
 TEMP_DIR=$(mktemp -d)
 git clone --depth 1 https://github.com/Yorushika-rs/termux-config.git "$TEMP_DIR"
 
-mkdir -p ~/.config/fish
+mkdir -p ~/.config/fish/functions
 cp -r "$TEMP_DIR/fish/"* ~/.config/fish/
 cp "$TEMP_DIR/.aliases" ~/.aliases
-
-# Copia el script de finalización a la raíz (~)
 cp "$TEMP_DIR/fish/finish_setup.fish" ~/finish_setup.fish
 
 rm -rf "$TEMP_DIR"
 
 chsh -s fish
-
-echo "Fase 1 completa. Ahora entra en fish y ejecuta: source ~/finish_setup.fish"
+fish
