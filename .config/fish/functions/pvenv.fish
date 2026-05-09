bc2e2440a776a18a6c8e5416b9c71984f7783a9c
Yorushika <108711210+Yorushika-rs@users.noreply.github.com>
2026-05-09 17:22:28 -0400

    Change default venv name from .venv to venv

--- .config/fish/functions/pvenv.fish
+++ .config/fish/functions/pvenv.fish
@@ -1,7 +1,7 @@
 function pvenv --argument venv_name
     # Si no das nombre, usa .venv por defecto
     if test -z "$venv_name"
-        set venv_name .venv
+        set venv_name venv
     end
 
     if test -d "./$venv_name"
