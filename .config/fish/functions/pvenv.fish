2662556329af41ff9c2ccd686fc78ea2c59c7e6d
Yorushika <108711210+Yorushika-rs@users.noreply.github.com>
2026-05-09 01:15:38 -0400

    Add pvenv function for virtual environment management
    Adds a function to create and activate a Python virtual environment.

---
+++ .config/fish/functions/pvenv.fish
@@ -0,0 +1,14 @@
+function pven --argument venv_name
+    # Si no das nombre, usa .venv por defecto
+    if test -z "$venv_name"
+        set venv_name .venv
+    end
+
+    if test -d "./$venv_name"
+        source "./$venv_name/bin/activate.fish"
+    else
+        echo "Creando entorno virtual: $venv_name"
+        python3 -m venv "$venv_name"
+        source "./$venv_name/bin/activate.fish"
+    end
+end
